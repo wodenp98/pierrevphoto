10ccfe4ff30d763f26346d4e5d103eb901d10bc3
Paul Vigneron <paulvigneron4698@gmail.com>
2023-12-07 15:14:24 +0100

    fix somes details before prod

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -49,13 +49,13 @@ export default async function Portfolio() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <button className="group mt-6 h-96 w-9/12">
+        <button className="group mt-6 h-96 w-3/5">
           <div className="relative overflow-hidden h-full cursor-pointer">
             <Image
               src="/assets/Nature.jpg"
               alt="Photo de pierre"
-              height={1080}
-              width={1920}
+              height={720}
+              width={1080}
               className="rounded-md col-span-1 w-full h-full  object-cover"
             />
             <div className="rounded-md absolute h-full w-full bg-black/50 flex items-center justify-center group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
